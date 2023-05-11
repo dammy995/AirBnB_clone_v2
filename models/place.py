253d97835341acbcf2348e7a4d20b7cd54bd6908
Damian <damianemerah1@gmail.com>
2023-05-11 22:38:25 +0100

    changes made

--- models/place.py
+++ models/place.py
@@ -27,7 +27,7 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     if storage_type == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        city_id = Column(String(60, collation='latin1_swedish_ci'), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
